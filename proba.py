4a22d91b16a398782dbce880020bb0cb94d7f6b1
V9balazs <9balazs@gmail.com>
2024-10-29 12:27:04 +0100

    fashion training limiter

--- proba.py
+++ proba.py
@@ -1,6 +1,16 @@
 import numpy as np
 import tensorflow as tf
 
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get("accuracy") >= 0.6:  # Experiment with changing this value
+            print("\nReached 60% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+
+callbacks = myCallback()
+
 fmnist = tf.keras.datasets.fashion_mnist
 
 (training_images, training_labels), (test_images, test_labels) = fmnist.load_data()
@@ -19,7 +29,7 @@ model = tf.keras.models.Sequential(
     [
         tf.keras.Input(shape=(28, 28)),
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(1024, activation=tf.nn.relu),
+        tf.keras.layers.Dense(512, activation=tf.nn.relu),
         tf.keras.layers.Dense(10, activation=tf.nn.softmax),
     ]
 )
@@ -28,20 +38,17 @@ inputs = np.array([[1.0, 3.0, 4.0, 2.0]])
 inputs = tf.convert_to_tensor(inputs)
 print(f"input to softmax function: {inputs.numpy()}")
 
-# Feed the inputs to a softmax activation function
 outputs = tf.keras.activations.softmax(inputs)
 print(f"output of softmax function: {outputs.numpy()}")
 
-# Get the sum of all values after the softmax
 sum = tf.reduce_sum(outputs)
 print(f"sum of outputs: {sum}")
 
-# Get the index with highest value
 prediction = np.argmax(outputs)
 print(f"class with highest probability: {prediction}")
 
-model.compile(optimizer=tf.optimizers.Adam(), loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
-model.fit(training_images, training_labels, epochs=5)
+model.fit(training_images, training_labels, epochs=15, callbacks=[callbacks])
 
 model.evaluate(test_images, test_labels)
