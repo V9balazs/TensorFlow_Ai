1c4c8f601a117a176831203b8f10a676362d9ccd
V9balazs <9balazs@gmail.com>
2024-11-15 10:20:04 +0100

    NLP token

--- proba.py
+++ proba.py
@@ -1,96 +1,40 @@
-import os
-import random
-from io import BytesIO
-
-import numpy as np
 import tensorflow as tf
 
-BASE_DIR = "./rps"
+# Sample inputs
+sentences = ["i love my dog", "I, love my cat"]
 
-rock_dir = os.path.join(BASE_DIR, "rock")
-paper_dir = os.path.join(BASE_DIR, "paper")
-scissors_dir = os.path.join(BASE_DIR, "scissors")
+# Initialize the layer
+vectorize_layer = tf.keras.layers.TextVectorization()
 
-print(f"total training rock images: {len(os.listdir(rock_dir))}")
-print(f"total training paper images: {len(os.listdir(paper_dir))}")
-print(f"total training scissors images: {len(os.listdir(scissors_dir))}")
+# Build the vocabulary
+vectorize_layer.adapt(sentences)
 
-rock_files = os.listdir(rock_dir)
-paper_files = os.listdir(paper_dir)
-scissors_files = os.listdir(scissors_dir)
+# Get the vocabulary list. Ignore special tokens for now.
+vocabulary = vectorize_layer.get_vocabulary(include_special_tokens=False)
 
-print()
-print(f"5 files in the rock subdir: {rock_files[:5]}")
-print(f"5 files in the paper subdir: {paper_files[:5]}")
-print(f"5 files in the scissors subdir: {scissors_files[:5]}")
+# Print the token index
+for index, word in enumerate(vocabulary):
+    print(index, word)
 
-model = tf.keras.models.Sequential(
-    [
-        tf.keras.Input(shape=(150, 150, 3)),
-        # Rescale the image. Note the input shape is the desired size of the image: 150x150 with 3 bytes for color
-        tf.keras.layers.Rescaling(1.0 / 255),
-        # This is the first convolution
-        tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
-        tf.keras.layers.MaxPooling2D(2, 2),
-        # The second convolution
-        tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
-        tf.keras.layers.MaxPooling2D(2, 2),
-        # The third convolution
-        tf.keras.layers.Conv2D(128, (3, 3), activation="relu"),
-        tf.keras.layers.MaxPooling2D(2, 2),
-        # The fourth convolution
-        tf.keras.layers.Conv2D(128, (3, 3), activation="relu"),
-        tf.keras.layers.MaxPooling2D(2, 2),
-        # Flatten the results to feed into a DNN
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dropout(0.5),
-        # 512 neuron hidden layer
-        tf.keras.layers.Dense(512, activation="relu"),
-        tf.keras.layers.Dense(3, activation="softmax"),
-    ]
-)
+# Add another input
+sentences = ["i love my dog", "I, love my cat", "You love my dog!"]
 
-# Print the model summary
-model.summary()
+# Initialize the layer
+vectorize_layer = tf.keras.layers.TextVectorization()
 
-TRAINING_DIR = "./rps"
-VALIDATION_DIR = "./rps-test-set"
+# Build the vocabulary
+vectorize_layer.adapt(sentences)
 
-# Instantiate the training dataset
-train_dataset = tf.keras.utils.image_dataset_from_directory(
-    TRAINING_DIR, image_size=(150, 150), batch_size=32, label_mode="categorical"
-)
+# Get the vocabulary list. Ignore special tokens for now.
+vocabulary = vectorize_layer.get_vocabulary(include_special_tokens=False)
 
-# Instantiate the validation dataset
-validation_dataset = tf.keras.utils.image_dataset_from_directory(
-    VALIDATION_DIR, image_size=(150, 150), batch_size=32, label_mode="categorical"
-)
+# Print the token index
+for index, word in enumerate(vocabulary):
+    print(index, word)
 
-# Optimize the datasets for training
-SHUFFLE_BUFFER_SIZE = 1000
-PREFETCH_BUFFER_SIZE = tf.data.AUTOTUNE
+# Get the vocabulary list.
+vocabulary = vectorize_layer.get_vocabulary()
 
-train_dataset_final = train_dataset.cache().shuffle(SHUFFLE_BUFFER_SIZE).prefetch(PREFETCH_BUFFER_SIZE)
-
-validation_dataset_final = validation_dataset.cache().prefetch(PREFETCH_BUFFER_SIZE)
-
-# Create a model with data augmentation layers
-data_augmentation = tf.keras.Sequential(
-    [
-        tf.keras.Input(shape=(150, 150, 3)),
-        tf.keras.layers.RandomFlip("horizontal"),
-        tf.keras.layers.RandomRotation(0.4),
-        tf.keras.layers.RandomTranslation(0.2, 0.2),
-        tf.keras.layers.RandomContrast(0.4),
-        tf.keras.layers.RandomZoom(0.2),
-    ]
-)
-
-# Attach the data augmentation model to the base model
-model_with_aug = tf.keras.models.Sequential([data_augmentation, model])
-
-# Set the training parameters
-model_with_aug.compile(loss="categorical_crossentropy", optimizer="rmsprop", metrics=["accuracy"])
-
-# Train the model
-history = model_with_aug.fit(train_dataset_final, epochs=25, validation_data=validation_dataset_final, verbose=1)
+# Print the token index
+for index, word in enumerate(vocabulary):
+    print(index, word)
