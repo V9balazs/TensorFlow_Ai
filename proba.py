01a93dab829b52c5d1488ad8dfa427b8b44b5be1
V9balazs <9balazs@gmail.com>
2024-11-28 11:57:34 +0100

    Time series data trainig sunspots

--- proba.py
+++ proba.py
@@ -1,121 +1,40 @@
+import csv
+
 import numpy as np
 import tensorflow as tf
 
+# Download the dataset
+!wget -nc https://storage.googleapis.com/tensorflow-1-public/course4/Sunspots.csv
 
-def plot_series(x, y, format="-", start=0, end=None, title=None, xlabel=None, ylabel=None, legend=None):
-    """
-    Visualizes time series data
+# Preview the dataset
+!head Sunspots.csv
 
-    Args:
-      x (array of int) - contains values for the x-axis
-      y (array of int or tuple of arrays) - contains the values for the y-axis
-      format (string) - line style when plotting the graph
-      start (int) - first time step to plot
-      end (int) - last time step to plot
-      title (string) - title of the plot
-      xlabel (string) - label for the x-axis
-      ylabel (string) - label for the y-axis
-      legend (list of strings) - legend for the plot
-    """
+# Initialize lists
+time_step = []
+sunspots = []
 
+# Open CSV file
+with open('./Sunspots.csv') as csvfile:
+  
+  # Initialize reader
+  reader = csv.reader(csvfile, delimiter=',')
+  
+  # Skip the first line
+  next(reader)
+  
+  # Append row and sunspot number to lists
+  for row in reader:
+    time_step.append(int(row[0]))
+    sunspots.append(float(row[2]))
 
-def trend(time, slope=0):
-    """
-    Generates synthetic data that follows a straight line given a slope value.
-
-    Args:
-      time (array of int) - contains the time steps
-      slope (float) - determines the direction and steepness of the line
-
-    Returns:
-      series (array of float) - measurements that follow a straight line
-    """
-
-    # Compute the linear series given the slope
-    series = slope * time
-
-    return series
-
-
-def seasonal_pattern(season_time):
-    """
-    Just an arbitrary pattern, you can change it if you wish
-
-    Args:
-      season_time (array of float) - contains the measurements per time step
-
-    Returns:
-      data_pattern (array of float) -  contains revised measurement values according
-                                  to the defined pattern
-    """
-
-    # Generate the values using an arbitrary pattern
-    data_pattern = np.where(season_time < 0.4, np.cos(season_time * 2 * np.pi), 1 / np.exp(3 * season_time))
-
-    return data_pattern
-
-
-def seasonality(time, period, amplitude=1, phase=0):
-    """
-    Repeats the same pattern at each period
-
-    Args:
-      time (array of int) - contains the time steps
-      period (int) - number of time steps before the pattern repeats
-      amplitude (int) - peak measured value in a period
-      phase (int) - number of time steps to shift the measured values
-
-    Returns:
-      data_pattern (array of float) - seasonal data scaled by the defined amplitude
-    """
-
-    # Define the measured values per period
-    season_time = ((time + phase) % period) / period
-
-    # Generates the seasonal data scaled by the defined amplitude
-    data_pattern = amplitude * seasonal_pattern(season_time)
-
-    return data_pattern
-
-
-def noise(time, noise_level=1, seed=None):
-    """Generates a normally distributed noisy signal
-
-    Args:
-      time (array of int) - contains the time steps
-      noise_level (float) - scaling factor for the generated signal
-      seed (int) - number generator seed for repeatability
-
-    Returns:
-      noise (array of float) - the noisy signal
-    """
-
-    # Initialize the random number generator
-    rnd = np.random.RandomState(seed)
-
-    # Generate a random number for each time step and scale by the noise level
-    noise = rnd.randn(len(time)) * noise_level
-
-    return noise
-
-
-# Parameters
-time = np.arange(4 * 365 + 1, dtype="float32")
-baseline = 10
-amplitude = 40
-slope = 0.05
-noise_level = 5
-
-# Create the series
-series = baseline + trend(time, slope) + seasonality(time, period=365, amplitude=amplitude)
-
-# Update with noise
-series += noise(time, noise_level, seed=42)
+# Convert lists to numpy arrays
+time = np.array(time_step)
+series = np.array(sunspots)
 
 # Define the split time
-split_time = 1000
+split_time = 3000
 
-# Get the train set
+# Get the train set 
 time_train = time[:split_time]
 x_train = series[:split_time]
 
@@ -123,78 +42,64 @@ x_train = series[:split_time]
 time_valid = time[split_time:]
 x_valid = series[split_time:]
 
-# Parameters
-window_size = 20
-batch_size = 16
-shuffle_buffer_size = 1000
-
-
 def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
     """Generates dataset windows
 
     Args:
       series (array of float) - contains the values of the time series
-      window_size (int) - the number of time steps to average
+      window_size (int) - the number of time steps to include in the feature
       batch_size (int) - the batch size
       shuffle_buffer(int) - buffer size to use for the shuffle method
 
     Returns:
       dataset (TF Dataset) - TF Dataset containing time windows
     """
-
-    # Add an axis for the feature dimension of RNN layers
-    series = tf.expand_dims(series, axis=-1)
-
+  
     # Generate a TF Dataset from the series values
     dataset = tf.data.Dataset.from_tensor_slices(series)
-
+    
     # Window the data but only take those with the specified size
     dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
-
+    
     # Flatten the windows by putting its elements in a single batch
     dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
 
-    # Create tuples with features and labels
+    # Create tuples with features and labels 
     dataset = dataset.map(lambda window: (window[:-1], window[-1]))
 
     # Shuffle the windows
     dataset = dataset.shuffle(shuffle_buffer)
-
+    
     # Create batches of windows
     dataset = dataset.batch(batch_size)
-
+    
     # Optimize the dataset for training
     dataset = dataset.cache().prefetch(1)
-
+    
     return dataset
 
+# Parameters
+window_size = 30
+batch_size = 32
+shuffle_buffer_size = 1000
 
 # Generate the dataset windows
 train_set = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
 
-# Reset states generated by Keras
-tf.keras.backend.clear_session()
-
 # Build the model
-model = tf.keras.models.Sequential(
-    [
-        tf.keras.Input(shape=(window_size, 1)),
-        tf.keras.layers.Conv1D(filters=64, kernel_size=3, strides=1, padding="causal", activation="relu"),
-        tf.keras.layers.LSTM(64, return_sequences=True),
-        tf.keras.layers.LSTM(64),
-        tf.keras.layers.Dense(1),
-        tf.keras.layers.Lambda(lambda x: x * 400),
-    ]
-)
+model = tf.keras.models.Sequential([
+    tf.keras.Input(shape=(window_size,)),
+    tf.keras.layers.Dense(30, activation="relu"), 
+    tf.keras.layers.Dense(10, activation="relu"),
+    tf.keras.layers.Dense(1)
+])
 
 # Print the model summary
 model.summary()
 
-# Get initial weights
-init_weights = model.get_weights()
-
 # Set the learning rate scheduler
-lr_schedule = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 1e-8 * 10 ** (epoch / 20))
+lr_schedule = tf.keras.callbacks.LearningRateScheduler(
+    lambda epoch: 1e-8 * 10**(epoch / 20))
 
 # Initialize the optimizer
 optimizer = tf.keras.optimizers.SGD(momentum=0.9)
@@ -205,31 +110,30 @@ model.compile(loss=tf.keras.losses.Huber(), optimizer=optimizer)
 # Train the model
 history = model.fit(train_set, epochs=100, callbacks=[lr_schedule])
 
-# Define the learning rate array
-lrs = 1e-8 * (10 ** (np.arange(100) / 20))
-
 # Reset states generated by Keras
 tf.keras.backend.clear_session()
 
-# Reset the weights
-model.set_weights(init_weights)
+# Build the Model
+model = tf.keras.models.Sequential([
+    tf.keras.Input(shape=(window_size,)),
+    tf.keras.layers.Dense(30, activation="relu"), 
+    tf.keras.layers.Dense(10, activation="relu"),
+    tf.keras.layers.Dense(1)
+])
 
 # Set the learning rate
-learning_rate = 1e-7
+learning_rate = 2e-5
 
-# Set the optimizer
+# Set the optimizer 
 optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate, momentum=0.9)
 
 # Set the training parameters
-model.compile(loss=tf.keras.losses.Huber(), optimizer=optimizer, metrics=["mae"])
+model.compile(loss=tf.keras.losses.Huber(),
+              optimizer=optimizer,
+              metrics=["mae"])
 
 # Train the model
-history = model.fit(train_set, epochs=500)
-
-# Get mae and loss from history log
-mae = history.history["mae"]
-loss = history.history["loss"]
-
+history = model.fit(train_set,epochs=100)
 
 def model_forecast(model, series, window_size, batch_size):
     """Uses an input model to generate predictions on data windows
@@ -244,9 +148,6 @@ def model_forecast(model, series, window_size, batch_size):
       forecast (numpy array) - array containing predictions
     """
 
-    # Add an axis for the feature dimension of RNN layers
-    series = tf.expand_dims(series, axis=-1)
-
     # Generate a TF Dataset from the series values
     dataset = tf.data.Dataset.from_tensor_slices(series)
 
@@ -255,25 +156,23 @@ def model_forecast(model, series, window_size, batch_size):
 
     # Flatten the windows by putting its elements in a single batch
     dataset = dataset.flat_map(lambda w: w.batch(window_size))
-
+    
     # Create batches of windows
     dataset = dataset.batch(batch_size).prefetch(1)
-
+    
     # Get predictions on the entire dataset
     forecast = model.predict(dataset, verbose=0)
-
+    
     return forecast
 
-
 # Reduce the original series
-forecast_series = series[split_time - window_size : -1]
+forecast_series = series[split_time-window_size:-1]
 
 # Use helper function to generate predictions
 forecast = model_forecast(model, forecast_series, window_size, batch_size)
 
-# Drop single dimensional axes
+# Drop single dimensional axis
 results = forecast.squeeze()
 
-## Compute the MAE and MSE
-print(tf.keras.metrics.mse(x_valid, results).numpy())
+# Compute the MAE
 print(tf.keras.metrics.mae(x_valid, results).numpy())
